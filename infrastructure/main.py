f68c2a11efe23e6840f6e908f273ca478ca1f1aa
alehuso <sherlynhuso09@gmail.com>
2025-12-05 21:53:26 -0500

    Add infrastructure main entrypoint and key vault setup

--- infrastructure/main.py
+++ infrastructure/main.py
@@ -1,2 +1,76 @@
-""" Main Pulumi Program - orchestrates all infrastructure"""
+import pulumi
+from platform.resource_group import create_resource_group
+from platform.storage import create_data_lake
+from platform.key_vault import create_key_vault
+from platform.databricks_ws import create_databricks_workspace
+from platform.data_factory import create_data_factory
 
+
+config = pulumi.Config()
+project_name = config.get("project_name") or "credicorp-de"
+enviroment = config.get("enviroment") or "dev"
+location = config.get("location") or "eastus2"
+
+def get_resource_name(resource_type: str) -> str:
+    return f"{resource_type}-{project_name}-{enviroment}"
+
+resource_group = create_resource_group(
+    name=get_resource_name("rg"),
+    location= location,
+    tags={
+        "Enviroment":enviroment,
+        "Project":project_name,
+        "ManageBy":"Pulumi",
+        "Owner":"DataEngineering"
+    }
+)
+
+
+storage_account = create_data_lake(
+    name=f"st{project_name.replace('-','')}{enviroment}",
+    resource_group_name=resource_group.name,
+    location=location
+)
+
+key_vault = create_key_vault(
+    name=get_resource_name("kv"),
+    resource_group_name=resource_group.name,
+    location=location
+)
+
+databricks_workspace = create_databricks_workspace(
+    name=get_resource_name("dbw"),
+    resource_group_name=resource_group.name,
+    location=location,
+    sku="standard"
+)
+
+data_factory = create_data_factory(
+    name=get_resource_name("adf"),
+    resource_group_name=resource_group.name,
+    location=location
+)
+
+pulumi.export("resource_group_name", resource_group.name)
+pulumi.export("storage_account_name", storage_account.name)
+pulumi.export("key_vault_name", key_vault.name)
+pulumi.export("key_vault_uri", key_vault.vault_uri)
+pulumi.export("databricks_workspace_url", databricks_workspace.workspace_url)
+pulumi.export("data_factory_name", data_factory.name)
+
+pulumi.export("deployment_summary", pulumi.Output.all(
+    resource_group.name,
+    storage_account.name,
+    databricks_workspace.workspace_url
+).apply(lambda args: f"""
+
+Resource Group:    {args[0]}
+Storage Account:   {args[1]}
+Databricks:        {args[2]}
+
+"""))
+
+#Next Steps:
+#1. Run: pulumi stack output storage_account_key
+#2. Upload data: python scripts/upload_to_datalake.py
+#3. Deploy notebooks: cd databricks && ./deploy.sh
